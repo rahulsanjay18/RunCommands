7bcf72894ef501f7eb7597b7fd44f440476fe915
Rahul Shah <rahulsanjay18@gmail.com>
2021-05-03 15:36:30 -0400

    added print statements to install

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
+echo "Copying .vimrc file..."
 cp -rf .vimrc ~/.vimrc
+
+echo "Copying .vim folder..."
 cp -rf .vim/ ~/.vim
+
+echo "Copying and sourcing .bashrc file..."
 cp -rf .bashrc ~/.bashrc
 source ~/.bashrc
